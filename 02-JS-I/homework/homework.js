7390e4b6c6b276234e2a005e5a6f161a809dff04
jhondacelis95 <88688717+jhondacelis95@users.noreply.github.com>
2021-08-16 13:05:27 -0500

    update

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -174,6 +174,8 @@ function esPositivo(numero) {
   }
 }
 
+//Holi
+
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
